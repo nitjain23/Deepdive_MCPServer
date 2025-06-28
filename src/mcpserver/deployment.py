1d46cbfe33b0c98ec942a63dbf7cdcc5c7603af2
nitjain23 <nitjain23@outlook.com>
2025-06-28 17:39:18 +0530

    add comments

--- src/mcpserver/deployment.py
+++ src/mcpserver/deployment.py
@@ -4,5 +4,12 @@ mcp = FastMCP("Package Demo")
 
 @mcp.tool()
 def addition(a: int, b: int) -> int:
-    """Adds two numbers together."""
+    """
+    Adds two numbers together.
+    Args:
+        a (int): The first number.
+        b (int): The second number.
+    Returns:
+        int: The sum of the two numbers.
+    """
     return a + b
